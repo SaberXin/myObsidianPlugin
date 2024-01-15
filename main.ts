9676dbd84ffe25b7b1bf191d29557be0700f40bb
archerxchen <archerxchen@tencent.com>
2024-01-15 20:00:36 +0800

    insert draw.io

--- main.ts
+++ main.ts
@@ -1,134 +1,66 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Editor, TFile } from 'obsidian';
+import * as child_process from 'child_process';
+import * as path from 'path';
 
-// Remember to rename these classes and interfaces!
+export default class DrawIOPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: 'open-draw-io',
+      name: 'Insert draw.io',
+      editorCallback: async (editor, view) => {
+        await this.insertDrawio(editor, view.file);
+      },
+    });
+  }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+  async insertDrawio(editor: Editor, file: TFile) {
+	if (!file) return;
+  
+	const attachmentsFolderName = 'attachments';
+  	const svgFileName = 'untitled.svg';
+  	const attachmentsPath = path.join(file.parent.path, attachmentsFolderName);
+  	const svgFilePath = path.join(attachmentsPath, svgFileName);
+  	const svgFileLink = `![[${attachmentsFolderName}/${svgFileName}]]`;
+  
+	// draw.io 空文件的基本内容
+	const EMPTY_DIAGRAM_SVG = `<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+	<svg
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	version="1.1" width="1px" height="1px"
+	viewBox="-0.5 -0.5 1 1"
+	content="&lt;mxGraphModel&gt;&lt;root&gt;&lt;mxCell id=&quot;0&quot;/&gt;&lt;mxCell id=&quot;1&quot; parent=&quot;0&quot;/&gt;&lt;/root&gt;&lt;/mxGraphModel&gt;">
+	</svg>`;
+  
+	try {
+	  // 检查attachments目录是否存在，如果不存在则创建
+	   if (!this.app.vault.getAbstractFileByPath(attachmentsPath)) {
+		await this.app.vault.createFolder(attachmentsPath);
+	  }	
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	  // 创建一个基本的draw.io文件
+	  await this.app.vault.create(svgFilePath, EMPTY_DIAGRAM_SVG);
+  
+	  // 在当前光标位置插入链接
+	  editor.replaceSelection(svgFileLink);
+  
+	  // 获取绝对路径
+	  const fullSvgFilePath = this.app.vault.adapter.getFullPath(svgFilePath);
+	  
+	  // 打印路径用于调试
+	  console.log("Opening file at path:", fullSvgFilePath);
+	  
+	  // 打开draw.io应用
+	  child_process.exec(`open -a draw.io.app "${fullSvgFilePath}"`, (err) => {
+	    if (err) {
+	      console.error('Error opening draw.io:', err);
+	    } else {
+	      console.log('draw.io opened successfully');
+	    }
+	  });
+	} catch (error) {
+	  console.error('Error handling draw.io file:', error);
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  }
 }
